9505306ccebcdd523b8548a5c411887033462789
Tanja Tornede <t.tornede@ai.uni-hannover.de>
2023-06-08 11:51:29 +0200

    Set up project

---
+++ bibtex-cleaner/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+
+name = "bibtex-cleaner"
+package_name = "bibtex-cleaner"
+author = "Tanja Tornede"
+author_email = "t.tornede@ai.uni-hannover.de"
+description = "Rephrases BibTeX entries to match the formatting guidelines."
+url = "https://www.automl.org"
+project_urls = {
+    "Source Code": "https://github.com/automl-private/bibtex-cleaner",
+}
+copyright = f"Copyright {datetime.date.today().strftime('%Y')}, Tanja Tornede"
+version = "0.0.1"
